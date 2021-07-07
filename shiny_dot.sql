26607c2364ca569355d200cf0a298abc5e2d99bc
CYL <68176915+Oliverlin67@users.noreply.github.com>
2021-07-07 12:05:13 +0800

    Add files via upload

---
+++ shiny_dot.sql
@@ -0,0 +1,282 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7deb1
+-- https://www.phpmyadmin.net/
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+08:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `shiny_dot`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `account`
+--
+
+CREATE TABLE `account` (
+  `id` int NOT NULL,
+  `instagram_same` tinyint(1) NOT NULL,
+  `user_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `user_pwd` varchar(800) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `user_group` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `user_status` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `user_medal` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+  `user_studientid` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `profile_name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `profile_nickname` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `profile_instagram` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `profile_birthday` date NOT NULL,
+  `profile_school` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `profile_bio` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `profile_mail` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `profile_sex` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `join_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `board`
+--
+
+CREATE TABLE `board` (
+  `id` int NOT NULL,
+  `board_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `title` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `board_logo` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `type` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `about` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `count` int NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `image`
+--
+
+CREATE TABLE `image` (
+  `id` int NOT NULL,
+  `fileid` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `uploader` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `like_post`
+--
+
+CREATE TABLE `like_post` (
+  `id` int NOT NULL,
+  `userid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `postid` int NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `post`
+--
+
+CREATE TABLE `post` (
+  `id` int NOT NULL,
+  `userid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `board_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `title` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `likeby` int NOT NULL DEFAULT '0',
+  `anonymous` tinyint(1) NOT NULL,
+  `align-top` tinyint(1) NOT NULL DEFAULT '0',
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `pwd_reset`
+--
+
+CREATE TABLE `pwd_reset` (
+  `id` int NOT NULL,
+  `userid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `reset_key` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `reset_at` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `qa`
+--
+
+CREATE TABLE `qa` (
+  `id` int NOT NULL,
+  `question` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `answer` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `rember`
+--
+
+CREATE TABLE `rember` (
+  `id` int NOT NULL,
+  `ip` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `user_info` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `enddate` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `reply`
+--
+
+CREATE TABLE `reply` (
+  `id` int NOT NULL,
+  `userid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `postid` int NOT NULL,
+  `content` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `anonymous` tinyint(1) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `setting`
+--
+
+CREATE TABLE `setting` (
+  `id` int NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `value` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `last_update` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- 傾印資料表的資料 `setting`
+--
+
+INSERT INTO `setting` (`id`, `name`, `value`, `last_update`) VALUES
+(1, 'forum_name', 'ShinyDot', '0000-00-00 00:00:00'),
+(2, 'forum_about', 'describe for this site', '0000-00-00 00:00:00'),
+(3, 'forum_logo', 'the logo url of this site', '0000-00-00 00:00:00'),
+(4, 'forum_privacy', 'the privacy for this site', '0000-00-00 00:00:00');
+
+--
+-- 資料表索引 `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `board`
+--
+ALTER TABLE `board`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `like_post`
+--
+ALTER TABLE `like_post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `pwd_reset`
+--
+ALTER TABLE `pwd_reset`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `reply`
+--
+ALTER TABLE `reply`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `setting`
+--
+ALTER TABLE `setting`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `board`
+--
+ALTER TABLE `board`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT,  AUTO_INCREMENT=1;
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `image`
+--
+ALTER TABLE `image`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT,  AUTO_INCREMENT=1;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `like_post`
+--
+ALTER TABLE `like_post`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT,  AUTO_INCREMENT=1;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT,  AUTO_INCREMENT=1;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `pwd_reset`
+--
+ALTER TABLE `pwd_reset`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `reply`
+--
+ALTER TABLE `reply`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT,  AUTO_INCREMENT=1;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `setting`
+--
+ALTER TABLE `setting`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
