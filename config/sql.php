f18ebe85840afd540151c211d8acbd70cf739e51
CYL <68176915+Oliverlin67@users.noreply.github.com>
2021-07-07 11:14:08 +0800

    Update sql.php

--- config/sql.php
+++ config/sql.php
@@ -1,6 +1,6 @@
 <?
 try{
-    $conn = new PDO("mysql:host=localhost;dbname=shiny_dot;charset=utf8mb4","shiny_dot","AEch9q%chpeDpki5@ki509qeD0LT00La855^TAE");
+    $conn = new PDO("mysql:host=localhost;dbname=shiny_dot;charset=utf8mb4","shiny_dot","XXXXXXXXXXXXXXXXXXX");
     $conn->setAttribute(PDO::ATTR_ERRMODE, 
                 PDO::ERRMODE_EXCEPTION);
     $conn->exec("SET time_zone = '+08:00'");
